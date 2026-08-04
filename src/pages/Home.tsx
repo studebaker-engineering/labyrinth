61bca29bdc9a9206c6489bb4a284de7e52cfed58
Bradon Studebaker <bradon@studebaker.engineering>
2026-08-04 12:01:25 -0400

    Add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,36 @@
 import { EntranceText } from "../components/EntranceText";
 
 export const Home = () => (
-  <div className="container p-5 mx-auto">
-    <section>
-      <h1>h1 Studebaker Engineering</h1>
-      <h2>h2 Studebaker Engineering</h2>
-      <h3>h3 Studebaker Engineering</h3>
-      <h4>h4 Studebaker Engineering</h4>
-      <h5>h5 Studebaker Engineering</h5>
-      <h6>h6 Studebaker Engineering</h6>
+	<div className="container p-5 mx-auto">
+		<section>
+			<h1>h1 Studebaker Engineering</h1>
+			<h2>h2 Studebaker Engineering</h2>
+			<h3>h3 Studebaker Engineering</h3>
+			<h4>h4 Studebaker Engineering</h4>
+			<h5>h5 Studebaker Engineering</h5>
+			<h6>h6 Studebaker Engineering</h6>
 
-      <br />
+			<br />
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.
+			</p>
 
-      <br />
+			<br />
 
-      <EntranceText />
-    </section>
-  </div>
+			<EntranceText />
+
+			<EntranceText />
+
+			<EntranceText />
+
+			<EntranceText />
+		</section>
+	</div>
 );
